9e04b3dcd11d8644af072ac93013d95f1d1a5739
azamgohar <144214140+azamgohar@users.noreply.github.com>
2026-04-07 21:26:59 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// declaration
+// intialization
+
+// const dulha = "Amir";
+// var dulhan = "Maria";
+// dulha = "Zubair";
+// dulha = "Junaid";
+// dulha = "Arman";
+// console.log(dulha, "weds", dulhan);
+
+// var raam = 4;
+// var shaam = 6;
+// console.log(raam+shaam, "mangoes");
+
+// const salary = 50000;
+
+// console.log(salary, "Harsh ko de diye hain.");
+
+// console.log("Maine 2 mahine mein hi", salary*2, "kama liya");
